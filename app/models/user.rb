ace3a2a759cf8b616ec1bc83f1520026c0f8b3d5
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-08-27 23:05:23 +0200

    [add] Sign in via OAuth with Spotify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:spotify]
+
+  def self.from_omniauth(auth)
+    where(email: auth.info['email']).first_or_create do |user|
+      user.email = auth.info['email']
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  require 'rspotify/oauth'
+  config.omniauth :spotify,
+  Rails.application.credentials[:spotify][:client_id],
+  Rails.application.credentials[:spotify][:client_secret],
+  scope: 'user-read-email',
+  callback_url: "http://localhost:3000/users/auth/spotify/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
