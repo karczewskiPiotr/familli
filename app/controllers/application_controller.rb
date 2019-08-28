077e5ed94fca0c10fc8e92df0dde6d3ddccb8096
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-08-28 17:39:18 +0200

    [add] app page, [add] improve redireect after authenticate_user! fail, [add] change redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(_resource)
+    app_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    root_path
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:app]
+
   def index; end
+
+  def app; end
 end
