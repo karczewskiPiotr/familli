4d685f8dd870f4f75c27392ab74303ed35cbd214
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-08-31 22:25:59 +0200

    [add] Animate remiders info.

--- app/javascript/pages/home/components/features/remiders.jsx
+++ app/javascript/pages/home/components/features/remiders.jsx
@@ -5,38 +5,40 @@ import Bell from "../../images/notify.svg";
 import Fade from "react-reveal/Fade";
 import Swing from "react-reveal/Swing";
 
-const Reminders = () => {
+const Reminders = ({ isVisible }) => {
   return (
-    <Row className="reminders">
-      <Col>
-        <Fade duration={2000}>
-          <div className="feature-description-wrapper">
-            <h2 className="header">No hussle remiders!</h2>
-            <p className="description">
-              Remidning members about their overdue payments has never been
-              easier. In Familli you are always one click away from sending them
-              email notifications!
-            </p>
-          </div>
-        </Fade>
-      </Col>
-      <Col>
-        <div className="behind-status-wrapper">
-          <div className="notification-bell">
-            <Fade right delay={900}>
-              <Swing forever={true} duration={2000}>
-                <img src={Bell} alt="Bell icon." />
-              </Swing>
-            </Fade>
-          </div>
-          <Fade right>
-            <div className="behind-status">
-              <img src={StatusBehind} alt="Member behind with payments" />
+    isVisible && (
+      <Row className="reminders">
+        <Col lg>
+          <Fade delay={300} duration={2000}>
+            <div className="feature-description-wrapper">
+              <h2 className="header">No hussle remiders!</h2>
+              <p className="description">
+                Remidning members about their overdue payments has never been
+                easier. In Familli you are always one click away from sending
+                them email notifications!
+              </p>
             </div>
           </Fade>
-        </div>
-      </Col>
-    </Row>
+        </Col>
+        <Col lg>
+          <div className="behind-status-wrapper">
+            <div className="notification-bell">
+              <Fade right delay={900}>
+                <Swing forever={true} duration={2000}>
+                  <img src={Bell} alt="Bell icon." />
+                </Swing>
+              </Fade>
+            </div>
+            <Fade right>
+              <div className="behind-status">
+                <img src={StatusBehind} alt="Member behind with payments" />
+              </div>
+            </Fade>
+          </div>
+        </Col>
+      </Row>
+    )
   );
 };
 
--- app/javascript/pages/home/components/home.jsx
+++ app/javascript/pages/home/components/home.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Logo from "../components/logo";
 import Message from "../components/message";
 import SignInButton from "../components/sign_in_button";
 import AppMock from "../components/app_mock";
-import Reminders from "../components/features/remiders"
+import Reminders from "../components/features/remiders";
+
+import VisibilitySensor from "react-visibility-sensor/visibility-sensor";
 
 const Home = () => {
+  const [isRemindersVisible, setIsRemindersVisible] = useState(false);
+
+  const toggleReminders = () => {
+    setIsRemindersVisible(visibility => {
+      return !visibility;
+    });
+  };
+
   return (
     <>
       <Container>
@@ -26,7 +36,10 @@ const Home = () => {
           </Col>
         </Row>
         <AppMock />
-        <Reminders />
+        <VisibilitySensor partialVisibility={true} onChange={toggleReminders}>
+          <div>&nbs</div>
+        </VisibilitySensor>
+        <Reminders isVisible={isRemindersVisible} />
       </Container>
     </>
   );
