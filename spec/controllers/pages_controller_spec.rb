b9bb2bdb2e13e1c8356911557d93cd619d97b2e4
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-01 11:48:33 +0200

    [fix] Fix tests after action name change.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  describe 'GET #index' do
-    before { get :index }
+  describe 'GET #home' do
+    before { get :home }
 
     describe 'successful response' do
       it { expect(response).to be_successful }
-      it { expect(response).to render_template('index') }
+      it { expect(response).to render_template('home') }
     end
   end
 end
