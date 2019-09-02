9924ee7566307c3fb4ce7fdb67ed4cbcacaaebfc
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-02 14:35:33 +0200

    [fix] optimize imports in home component

--- app/javascript/pages/home/components/home.jsx
+++ app/javascript/pages/home/components/home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Logo from "../components/logo";
-import Message from "../components/message";
-import SignInButton from "../components/sign_in_button";
-import AppMock from "../components/app_mock";
+import Logo from "./logo";
+import Message from "./message";
+import SignInButton from "./sign_in_button";
+import AppMock from "./app_mock";
 import Reminders from "./remiders";
 import Statuses from "./statuses";
 
