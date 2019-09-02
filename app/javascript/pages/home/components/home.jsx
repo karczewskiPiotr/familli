a0ad2696a5b03ae2525790e82de81e6ccfe318cf
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-02 19:36:50 +0200

    [fix] Optimize imports.

--- app/javascript/pages/home/components/home.jsx
+++ app/javascript/pages/home/components/home.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { Container, Row, Col, FormControl } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Logo from "./logo";
 import Message from "./message";
 import SignInButton from "./sign_in_button";
 import AppMock from "./app_mock";
 import Reminders from "./remiders";
 import Statuses from "./statuses";
-import Footer from "./footer"
+import Footer from "./footer";
 
 const Home = () => {
   return (
