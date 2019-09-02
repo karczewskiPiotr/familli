af58273ca1b0a65a7993f2db3d32fc471fc4ba85
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-02 19:33:50 +0200

    [add] minimalist footer

--- app/javascript/pages/home/components/home.jsx
+++ app/javascript/pages/home/components/home.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, FormControl } from "react-bootstrap";
 import Logo from "./logo";
 import Message from "./message";
 import SignInButton from "./sign_in_button";
 import AppMock from "./app_mock";
 import Reminders from "./remiders";
 import Statuses from "./statuses";
+import Footer from "./footer"
 
 const Home = () => {
   return (
@@ -29,6 +30,7 @@ const Home = () => {
         <AppMock />
         <Reminders />
         <Statuses />
+        <Footer />
       </Container>
     </>
   );
