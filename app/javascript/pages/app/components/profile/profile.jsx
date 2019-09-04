c9fb6aa910cc7cc8c0057230ca93e6806e71e320
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-04 14:33:42 +0200

    [fix] reconfigure catch all route to catch only /app/* matches

--- app/javascript/pages/app/components/profile/profile.jsx
+++ app/javascript/pages/app/components/profile/profile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <>
+      <h1>Profile</h1>
+      <a href="/users/sign_out" data-method="delete">
+        Log out!
+      </a>
+    </>
+  );
 };
 
 export default Profile;
