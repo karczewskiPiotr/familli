8fd0b4bdb80e2b4bcbb10dd2c2ff6593835f6f76
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-09-10 18:24:07 +0200

    [add] validation for familly model

--- spec/models/familly_spec.rb
+++ spec/models/familly_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Familly, type: :model do
     it { is_expected.to have_many(:invitations) }
     it { is_expected.to have_many(:members).through(:invitations) }
   end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:subscription_fee) }
+    it { is_expected.to validate_presence_of(:renewal_date) }
+    it { is_expected.to validate_presence_of(:currency) }
+  end
 end
