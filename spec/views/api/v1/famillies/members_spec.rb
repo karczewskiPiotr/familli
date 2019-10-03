bf7b4b7d14c0d0c7bcbc6bb65304d9fb6a5d1b98
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-10-03 11:17:41 +0200

    [add] Tests form accessing members data via api

--- spec/views/api/v1/famillies/members_spec.rb
+++ spec/views/api/v1/famillies/members_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe '/api/v1/famillies/members', type: :view do
+  let!(:members) { create_list(:user, 3) }
+  let!(:user) { create(:user, :parent) }
+
+  before do
+    sign_in(user)
+    @members = members
+    render template: '/api/v1/famillies/members'
+  end
+
+  describe 'correct response' do
+    subject(:data) { JSON.parse(response)['data'] }
+
+    it { expect(data.length).to eq(3) }
+    it 'attributes' do
+      data.each do |member|
+        expect(member.keys).to match_array(%w[identity email status profile_image])
+      end
+    end
+  end
 end
