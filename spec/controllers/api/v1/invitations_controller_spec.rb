6c9039438d65d9faa52827a5ed1ac6c76dce9f8e
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-01-21 21:07:38 +0100

    [fix] rubocop ofenses

--- spec/controllers/api/v1/invitations_controller_spec.rb
+++ spec/controllers/api/v1/invitations_controller_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe Api::V1::InvitationsController, type: :controller do
   let!(:user2) { create(:user) }
 
   describe 'POST #create' do
-    
     before do
-      sign_in(user) 
+      sign_in(user)
       user.famillies.create(attributes_for(:familly))
       Invitation.create(user_id: user.id, familly_id: user.famillies.first.id, status: :accepted)
     end
-    
+
+    let(:valid_attributes) { { invitation: { user_id: user2.id, familly_id: user.familly.id } } }
+
     context 'with valid attributes' do
-      let(:valid_attributes) { { invitation: { user_id: user2.id, familly_id: user.familly.id } } }
       subject(:api_call) { post :create, params: valid_attributes }
 
       it { expect { api_call }.to change(Invitation, :count).by(1) }
 
       it 'makes invitation status pending' do
         api_call
-        expect(Invitation.last.status).to eq("pending")
+        expect(Invitation.last.status).to eq('pending')
       end
     end
   end
